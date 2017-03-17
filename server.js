a9b4fd0f75c3e4a02f28e4ce4a6883dfd5c2db5d
Ma <n2603a@us.lmco.com>
2017-03-17 16:31:16 -0400

    demo v0.01

--- server.js
+++ server.js
@@ -1,44 +1,57 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + "/app"));
 app.use(express.static(__dirname + "/public"));
 
 app.use(function(req, res){
-    res.sendFile("index.html");
+    res.sendFile(__dirname + "/app/index.html");
 });
 
 var FeedSub = require('feedsub');
 var rssMap = new Map();
 var urls = [
-    'http://rss.cnn.com/rss/cnn_latest.rss',
-    'https://www.uscreditcardguide.com/en/feed/',
-    'http://lorem-rss.herokuapp.com/feed'
+    // 'http://rss.cnn.com/rss/cnn_latest.rss',
+    // 'https://www.uscreditcardguide.com/en/feed/',
+    'http://lorem-rss.herokuapp.com/feed?unit=second&interval=30'
 ];
 
-var runRSSSubServer = function(urls) {
-    urls.forEach(function(url){
-        var feedSub = new FeedSub(url, {
-            interval: 1,
-            emitOnStart: true,
-            forceInterval: true,
-            autoStart: true 
+io.on('connection', function(socket){
+
+    var runRSSSubServer = function(urls) {
+        urls.forEach(function(url){
+            var feedSub = new FeedSub(url, {
+                interval: 1,
+                emitOnStart: true,
+                forceInterval: true,
+                autoStart: true 
+            });
+            feedSub.on('item', function(item) {
+                io.emit("new contents", item);
+            });
+            rssMap.set(url, feedSub);
         });
-        feedSub.on('item', function(item) {
-            console.dir(item.title);
+    }
+
+    var stopRSSSubServer = function(urls) {
+        urls.forEach(function(url){
+            rssMap.get(url).stop();
         });
-        rssMap.set(url, feedSub);
+    }
+
+    runRSSSubServer(urls);    
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
     });
-}
 
-var stopRSSSubServer = function(urls) {
-    urls.forEach(function(url){
-        rssMap.get(url).stop();
+    socket.on('delete notify', function(index){
+        newContentList.splice(index, 1);
     });
-}
+});
 
-runRSSSubServer(urls);
-
-
-app.listen(8123);
+http.listen(8123);
 console.log("Server stated")
